0c770e8de2c0fa7bba75717df69953280a8b3f92
Lohith Reddy Bodumallu <lohithreddyb@gmail.com>
2026-04-22 13:23:03 +0530

    Update recognizer.py

--- face_auth/recognizer.py
+++ face_auth/recognizer.py
@@ -1,19 +1,13 @@
 from deepface import DeepFace
+from face_auth.database import known_users
 
 distance_threshold = 0.4
 
-def verify_face(face_region, reference_img_path):
-    try:
-        result = DeepFace.verify(
-            face_region,
-            reference_img_path,
-            enforce_detection=False
-        )
+def verify_against_db(face_region):
+    for name, path in known_users.items():
+        result = DeepFace.verify(face_region, path, enforce_detection=False)
 
         if result["verified"] and result["distance"] < distance_threshold:
-            return True, result["distance"]
-        return False, result["distance"]
+            return name, result["distance"]
 
-    except Exception as e:
-        print(f"DeepFace Error: {e}")
-        return False, None
+    return None, None
