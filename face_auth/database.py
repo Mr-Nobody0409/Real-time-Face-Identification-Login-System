2d5d06d22358e3d9d3b2a653eb3a9622c1e90646
Lohith Reddy Bodumallu <lohithreddyb@gmail.com>
2026-04-22 14:31:10 +0530

    Update database.py

--- face_auth/database.py
+++ face_auth/database.py
@@ -2,9 +2,16 @@ import os
 
 DATASET_PATH = "dataset/train"
 
-known_users = {}
+def load_users():
+    users = {}
+    if not os.path.exists(DATASET_PATH):
+        return users
 
-for file in os.listdir(DATASET_PATH):
-    if file.endswith(".jpg"):
-        name = file.split(".")[0]
-        known_users[name] = os.path.join(DATASET_PATH, file)
+    for file in os.listdir(DATASET_PATH):
+        if file.endswith(".jpg"):
+            name = file.split(".")[0]
+            users[name] = os.path.join(DATASET_PATH, file)
+
+    return users
+
+known_users = load_users()
