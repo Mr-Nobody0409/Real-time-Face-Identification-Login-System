daf39c409091232eff1e6ce02da46f722adcaa96
Lohith Reddy Bodumallu <lohithreddyb@gmail.com>
2026-04-22 14:01:39 +0530

    Update recognizer.py

--- face_auth/recognizer.py
+++ face_auth/recognizer.py
@@ -2,10 +2,10 @@ from deepface import DeepFace
 from face_auth.database import known_users
 
 def verify_user(face_region):
-    for name, img_path in known_users.items():
-        result = DeepFace.verify(face_region, img_path, enforce_detection=False)
+    for name, path in known_users.items():
+        result = DeepFace.verify(face_region, path, enforce_detection=False)
 
         if result["verified"] and result["distance"] < 0.4:
             return name
 
-    return None
+    return "Unknown"
